81f5b2782dfa3a2a64cccb8ab0791feeb1239510
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-07-26 23:30:53 +0600

    'Add' test created with 'Fact'

---
+++ DemoLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class CalculatorTests
+    {
+
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            // Arrange
+
+            double expected = 5;
+
+            // Act
+
+            double actual = Calculator.Add(3,2);
+
+            // Assert
+
+            Assert.Equal(expected, actual);
+
+
+        }
+
+    }
+}
