811576e997c13128e4e049a8a6b1f1741692f323
tawhidur-tanim <rahman35-964@diu.edu.bd>
2023-07-28 14:15:23 +0600

    example file test added

---
+++ DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+
+        [Fact]
+        public void ExampleLoadTextFile_ValidFileNameGiven()
+        {
+            // act
+            string actual = Examples.ExampleLoadTextFile("This is valid filename.");
+
+            // assert
+            Assert.True(actual.Length >  0);
+        }
+
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidFileNameGiven()
+        {
+            // act & assert
+
+            Assert.Throws<ArgumentException>("file",() => Examples.ExampleLoadTextFile(""));
+
+
+        }
+    }
+}
